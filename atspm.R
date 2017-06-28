fc03059587732b639144279c3e41f94ca1bc98e8
atops <alan.toppen@gmail.com>
2017-06-28 14:07:46 -0400

    Cleaned up colors code

--- atspm.R
+++ atspm.R
@@ -1,15 +1,13 @@
 library(dplyr)
 library(feather)
-library(RColorBrewer)
+#library(RColorBrewer)
 
-pal <- colorFactor(append("gray50", brewer.pal(6, "Set1")), c(paste("Zone", seq_len(6)), "Other"))
-cols <- c("#7F7F7F", "#FF7F00", "#FF7F00", "#4DAF4A", "#377EB8", "#E41A1C", "#984EA3")
-          # gray50,    yellow,    orange,    green,     blue,      red,       purple
-
+#pal <- colorFactor(append("gray50", brewer.pal(6, "Set1")), c(paste("Zone", seq_len(6)), "Other"))
+# Colors are from ColorBrewer Palette "Set1"
 cols <- c("#7F7F7F", "#E41A1C", "#FF7F00", "#4DAF4A", "#A65628", "#377EB8", "#984EA3")
-#           g - oth   r - 1       o - 2      g - 3      br- 4      bl- 5      p - 6      gray -other
-
+#         gray - oth  red - 1    ora - 2    grn - 3    brn- 4     blu- 5     pur - 6 
 pal <- colorFactor(cols, c(paste("Zone", seq_len(6)), "Other"))
+
 # KEEP BUT ONLY RUN WHEN MAXVIEW INTERSECTIONS EXCEL FILE CHANGES
 # signals_df <- readxl::read_xlsx("../MaxView Intersections.xlsx") %>% 
 #         transmute(SignalID, 
@@ -51,10 +49,3 @@ filter_df <- function(df, signal_id, dr) {
                    YellowTime < 6 & 
                    GreenTime < 100)
 }
-
-
-ints %>% 
-        transmute(SignalID, 
-                  Intersection = ifelse(is.na(SecondaryName), PrimaryName, paste(sep=" & ", PrimaryName, SecondaryName)), 
-                  Zone = ifelse(Zone=="DELETE", "Other", Zone), 
-                  Latitude, Longitude) %>% mutate(Zone = as.factor(Zone))
